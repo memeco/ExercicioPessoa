36547347a3c4135d57c38b8a2143da2f2859ef35
Emerson Marques Pedro <emersonmp@gmail.com>
2024-04-04 21:33:11 -0300

    Adicionei Classe

---
+++ ExercicioPessoa/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Pessoa
+{
+    private string nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(string nome, int idade, double altura)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Idade: {idade} anos");
+        Console.WriteLine($"Altura: {altura:F2} m");
+    }
+}
--- ExercicioPessoa/Program.cs
+++ ExercicioPessoa/Program.cs
@@ -2,27 +2,6 @@
 
 namespace ExercicioPessoa
 {
-    public class Pessoa
-    {
-        private string nome;
-        private int idade;
-        private double altura;
-
-        public Pessoa(string nome, int idade, double altura)
-        {
-            this.nome = nome;
-            this.idade = idade;
-            this.altura = altura;
-        }
-
-        public void Apresentar()
-        {
-            Console.WriteLine($"Nome: {nome}");
-            Console.WriteLine($"Idade: {idade} anos");
-            Console.WriteLine($"Altura: {altura:F2} m");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
